310f3285afc5c41e410b8d0d68ada68a2b903690
Nithyasri Babu <nithyasri.babu@gmail.com>
2020-11-07 07:31:02 -0500

    Challenge like and dislike

--- innovatehub/hub/views.py
+++ innovatehub/hub/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from hub.models import Challenge
+from django.http import JsonResponse
+
+from django.views.decorators.http import require_http_methods
+
+@require_http_methods(["POST"])
+def likeChallenge(request):
+    challengeID = request.POST.get('id')
+
+    challenge = Challenge.objects.get(pk=challengeID)
+    challenge.likes += 1
+    challenge.save()
+
+    return JsonResponse({'likes': challenge.likes})
+
+@require_http_methods(["POST"])
+def dislikeChallenge(request):
+    challengeID = request.POST.get('id')    
+
+    challenge = Challenge.objects.get(pk=challengeID)
+    challenge.dislikes += 1
+    challenge.save()
+
+    return JsonResponse({'dislikes': challenge.dislikes})
--- innovatehub/innovatehub/urls.py
+++ innovatehub/innovatehub/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import hub.views as views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('likechallenge/', views.likeChallenge, name='like'),
+    path('dislikechallenge/', views.dislikeChallenge, name='dislike')
 ]
