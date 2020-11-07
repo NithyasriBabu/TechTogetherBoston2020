f18eeb116dc640dd1716acea0844cd31733dafa9
Nithyasri Babu <nithyasri.babu@gmail.com>
2020-11-07 04:36:47 -0500

    Firebase Config Added

--- innovatehub/hub/admin.py
+++ innovatehub/hub/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from hub import models
 
 # Register your models here.
+
+admin.site.register(models.Skill)
+admin.site.register(models.UserProfile)
+admin.site.register(models.Challenge)
+admin.site.register(models.ChallengeAttachments)
+admin.site.register(models.ChallengeCategory)
+admin.site.register(models.Project)
+admin.site.register(models.ProjectContributers)
+admin.site.register(models.ProjectAttachments)
