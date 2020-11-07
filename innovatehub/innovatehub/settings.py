bbd60371b7aff8132dc1cb6ead576947657fac59
Nithyasri Babu <nithyasri.babu@gmail.com>
2020-11-07 03:06:04 -0500

    Models created

--- innovatehub/innovatehub/settings.py
+++ innovatehub/innovatehub/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'hub.apps.HubConfig',
 ]
 
 MIDDLEWARE = [
@@ -153,3 +154,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
